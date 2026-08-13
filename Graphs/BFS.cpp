722f3fde819f4e93eb9afd1a135236068f63ff5f
Mohitsoni-123 <mohitsonikadel2006@gmail.com>
2026-08-13 19:25:12 +0530

    Add BFS.cpp for adjacency list representation
    Implement BFS adjacency list representation.

---
+++ Graphs/BFS.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> adj(n);
+
+    for(int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    // Print Adjacency List
+    for(int i = 0; i < n; i++) {
+        cout << i << " -> ";
+
+        for(int neighbour : adj[i]) {
+            cout << neighbour << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
