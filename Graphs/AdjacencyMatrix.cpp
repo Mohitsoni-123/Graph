3f1a06f50d83d69d26fc3872c6ee1076f055a367
Mohitsoni-123 <mohitsonikadel2006@gmail.com>
2026-08-13 19:27:29 +0530

    Add Adjacency Matrix implementation in C++

---
+++ Graphs/AdjacencyMatrix.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> adj(n, vector<int>(n, 0));
+
+    for(int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+
+        adj[u][v] = 1;
+        adj[v][u] = 1;
+    }
+
+    // Print Adjacency Matrix
+    for(int i = 0; i < n; i++) {
+
+        for(int j = 0; j < n; j++) {
+            cout << adj[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
